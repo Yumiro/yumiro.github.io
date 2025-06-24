edf6ee59370c9cb0f864ea7949ff0599d9dca942
flag <38012125+Yumiro@users.noreply.github.com>
2025-06-24 21:44:13 +0100

    Blogpost #1 + tidy stuff up

--- script.js
+++ script.js
@@ -5,10 +5,10 @@
   add your own scripts to do whatever you like!*/
 
 //TABLE OF CONTENTS
-  // 1. Basic Info
-  // 2. Posts Array
-  // 3. Creating HTML Sections to Be Inserted (Header, Footer, etc)
-  // 4. Inserting the Sections Into our Actual HTML Pages
+// 1. Basic Info
+// 2. Posts Array
+// 3. Creating HTML Sections to Be Inserted (Header, Footer, etc)
+// 4. Inserting the Sections Into our Actual HTML Pages
 
 //-----------------------------
 
@@ -31,10 +31,10 @@ let authorLink = "https://flaf.dev/about"; // Enter your website, social media, 
   use a date it must still follow that format.*/
 
 let postsArray = [
-//[ "posts/2020-11-10-Special-Characters-Example.html", encodeURI( 'Spéci@l "Character\'s" Examp|e' ) ],
-//[ "posts/2020-11-10-My-Third-Post-Example.html" ],
-//[ "posts/2020-11-10-My-Second-Post-Example.html" ],
-[ "" ]
+  //[ "posts/2020-11-10-Special-Characters-Example.html", encodeURI( 'Spéci@l "Character\'s" Examp|e' ) ],
+  //[ "posts/2020-11-10-My-Third-Post-Example.html" ],
+  //[ "posts/2020-11-10-My-Second-Post-Example.html" ],
+  ["posts/2025-06-24-Purpose.html"],
 ];
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
@@ -56,7 +56,7 @@ const postDateFormat = /\d{4}\-\d{2}\-\d{2}\-/;
 
 //Check if you are in posts (if so, the links will have to go up a directory)
 let relativePath = ".";
-if ( url.includes("posts/") ) {
+if (url.includes("posts/")) {
   relativePath = "..";
 }
 
@@ -67,18 +67,18 @@ let headerHTML = `<ul><li><a href="${relativePath}/">home</a></li>
 
 //Generate the Footer HTML, which uses the variables defined in the BASIC INFO section above to list info about the site.
 //Note: feel free to remove the references to Zonelets and Neocities! Just be careful not to delete any necessary HTML closing tags or other syntax.
-let footerHTML = `<hr><p>${blogName} is written by <a href="${authorLink}">${authorName}</a>, using <a href="https://zonelets.net/">Zonelets</a> as a base and hosted through <a href="https://github.com/Yumiro/yumiro.github.io">GitHub</a><br>still under construction!`
+let footerHTML = `<hr><p>${blogName} is written by <a href="${authorLink}">${authorName}</a>, using <a href="https://zonelets.net/">Zonelets</a> as a base and hosted through <a href="https://github.com/Yumiro/yumiro.github.io">GitHub</a><br>still under construction!`;
 
 //Badges/Banners/whatever HTML
 let badgesHTML = `<hr><a class="no-hover" href="/"><img src="src/img/banner.png" class="img-container"></a>`;
 
 //To do the following stuff, we want to know where we are in the posts array (if we're currently on a post page).
 let currentIndex = -1;
-let currentFilename = url.substring(url.lastIndexOf('posts/'));
+let currentFilename = url.substring(url.lastIndexOf("posts/"));
 
 //Depending on the web server settings (Or something?), the browser url may or may not have ".html" at the end. If not, we must add it back in to match the posts array. (12-19-2022 fix)
 if (!currentFilename.endsWith(".html")) {
-    currentFilename += ".html";
+  currentFilename += ".html";
 }
 let i;
 for (i = 0; i < postsArray.length; i++) {
@@ -94,10 +94,10 @@ function formatPostTitle(i) {
   if (postsArray[i].length > 1) {
     //Remember how we had to use encodeURI for special characters up above? Now we use decodeURI to get them back.
     return decodeURI(postsArray[i][1]);
-  } else { 
-  //If there is no alternate post title, check if the post uses the date format or not, and return the proper title
-	if (postDateFormat.test(postsArray[i][0].slice(6, 17))) {
-	  return postsArray[i][0].slice(17, -5).replace(/-/g, " ");
+  } else {
+    //If there is no alternate post title, check if the post uses the date format or not, and return the proper title
+    if (postDateFormat.test(postsArray[i][0].slice(6, 17))) {
+      return postsArray[i][0].slice(17, -5).replace(/-/g, " ");
     } else {
       return postsArray[i][0].slice(6, -5).replace(/-/g, " ");
     }
@@ -114,19 +114,35 @@ if (currentIndex > -1) {
   if (postDateFormat.test(postsArray[currentIndex][0].slice(6, 17))) {
     let monthSlice = postsArray[currentIndex][0].slice(11, 13);
     let month = "";
-    if (monthSlice === "01") { month = "Jan";}
-    else if (monthSlice === "02") { month = "Feb"; }
-    else if (monthSlice === "03") { month = "Mar"; }
-    else if (monthSlice === "04") { month = "Apr"; }
-    else if (monthSlice === "05") { month = "May"; }
-    else if (monthSlice === "06") { month = "Jun"; }
-    else if (monthSlice === "07") { month = "Jul"; }
-    else if (monthSlice === "08") { month = "Aug"; }
-    else if (monthSlice === "09") { month = "Sep"; }
-    else if (monthSlice === "10") { month = "Oct"; }
-    else if (monthSlice === "11") { month = "Nov"; }
-    else if (monthSlice === "12") { month = "Dec"; }
-	niceDate = `${postsArray[currentIndex][0].slice(14, 16)} ${month}, ${postsArray[currentIndex][0].slice(6, 10)}`;
+    if (monthSlice === "01") {
+      month = "Jan";
+    } else if (monthSlice === "02") {
+      month = "Feb";
+    } else if (monthSlice === "03") {
+      month = "Mar";
+    } else if (monthSlice === "04") {
+      month = "Apr";
+    } else if (monthSlice === "05") {
+      month = "May";
+    } else if (monthSlice === "06") {
+      month = "Jun";
+    } else if (monthSlice === "07") {
+      month = "Jul";
+    } else if (monthSlice === "08") {
+      month = "Aug";
+    } else if (monthSlice === "09") {
+      month = "Sep";
+    } else if (monthSlice === "10") {
+      month = "Oct";
+    } else if (monthSlice === "11") {
+      month = "Nov";
+    } else if (monthSlice === "12") {
+      month = "Dec";
+    }
+    niceDate = `${postsArray[currentIndex][0].slice(
+      14,
+      16
+    )} ${month}, ${postsArray[currentIndex][0].slice(6, 10)}`;
   }
 }
 
@@ -137,16 +153,18 @@ function formatPostLink(i) {
   if (postsArray[i].length > 1) {
     postTitle_i = decodeURI(postsArray[i][1]);
   } else {
-	if (postDateFormat.test(postsArray[i][0].slice(6, 17))) {
-	  postTitle_i = postsArray[i][0].slice(17, -5).replace(/-/g, " ");
+    if (postDateFormat.test(postsArray[i][0].slice(6, 17))) {
+      postTitle_i = postsArray[i][0].slice(17, -5).replace(/-/g, " ");
     } else {
-      postTitle_i = "nothing yet! check back later, perhaps" //postsArray[i][0].slice(6, -5).replace(/-/g, " ");
+      postTitle_i = "nothing yet! check back later, perhaps"; //postsArray[i][0].slice(6, -5).replace(/-/g, " ");
     }
   }
   if (postDateFormat.test(postsArray[i][0].slice(6, 17))) {
-    return `<li><a href="${relativePath}/${postsArray[i][0]}">${postsArray[i][0].slice(6,16)} \u00BB ${postTitle_i}</a></li>`;
+    return `<li><a href="${relativePath}/${postsArray[i][0]}">${postsArray[
+      i
+    ][0].slice(6, 16)} \u00BB ${postTitle_i}</a></li>`;
   } else {
-    return postTitle_i //`<li><a href="${relativePath}/${postsArray[i][0]}">${postTitle_i}</a></li>`;
+    return postTitle_i; //`<li><a href="${relativePath}/${postsArray[i][0]}">${postTitle_i}</a></li>`;
   }
 }
 
@@ -239,14 +257,18 @@ if (document.title === "Blog Post") {
 
 // very ugly hacky solution to add .html to the end of links when working locally... i guess!!
 [...document.getElementsByTagName("a")].forEach((element) => {
-  if (element.href.includes("127.0.0.1") && !element.href.endsWith("/") && !element.href.endsWith(".html")) {
+  if (
+    element.href.includes("127.0.0.1") &&
+    !element.href.endsWith("/") &&
+    !element.href.endsWith(".html")
+  ) {
     element.href += ".html";
   }
 });
 
 // dummy
 console.log(
-                "%c ≽^•⩊•^≼ %c",
-                "background-color: #ff9200; color: black; font-weight: bold; border-radius: 8px;",
-                "font-style: italic; color: white;",
-            );
+  "%c ≽^•⩊•^≼ %c",
+  "background-color: #ff9200; color: black; font-weight: bold; border-radius: 8px;",
+  "font-style: italic; color: white;"
+);
